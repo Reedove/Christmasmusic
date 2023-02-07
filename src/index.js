7a7bd3239acdec578716f46ec8d51d0609c30cf0
Reedove <reehannejamawork@gmail.com>
2023-02-07 05:12:34 +0000

    ok2

--- src/index.js
+++ src/index.js
@@ -1,171 +1,251 @@
-$(function() {
-    $('.jcarousel').jcarousel({
-			wrap: 'circular'
-    });
-	
-		$('.jcarousel-pagination').jcarouselPagination({
-        item: function(page) {
-            return '<li id="jcarousel-item' + page + '"><a href="#' + page + '">' + page + '</a></li>';
-        }
-    });
-	
-		$('#jcarousel-item1').addClass('active');
-	
-		$('.jcarousel-pagination')
-    .on('jcarouselpagination:active', 'li', function() {
-        $(this).addClass('active');
-    })
-    .on('jcarouselpagination:inactive', 'li', function() {
-        $(this).removeClass('active');
-    });
-});
-
-$('.jcarousel-prev').click(function() {
-    $('.jcarousel').jcarousel('scroll', '-=1');
-	
-		$(".audio-alliwantforchristmas")[0].pause();
-		$(".audio-santababy")[0].pause();
-		$(".audio-hollyjollychristmas")[0].pause();
-		$(".play i").removeClass("fa-pause").addClass("fa-play");
-		$(".play").removeClass("active")
-	
-		if ($('#jcarousel-item1').hasClass('active')) {
-		$('.santababy, .avalanche').removeClass('active');
-		$('.the-alliwantforchristmas').addClass('active');
-		$(".song").html("alliwantforchristmas");
-		$(".artist").html("MariahCarey");
-		$(".duration").html($(".audio-alliwantforchristmas").duration);
-	};
-	
-	if ($('#jcarousel-item2').hasClass('active')) {
-		$('.alliwantforchristmas, .hollyjollychirstmas').removeClass('active');
-		$('.santababy').addClass('active');
-		$(".song").text("santababy");
-		$(".artist").text("ErhtaKitt");
-	};
-	
-	if ($('#jcarousel-item3').hasClass('active')) {
-		$('.alliwantforchirstmas, .santababy').removeClass('active');
-		$('.hollyjollychristams').addClass('active');
-		$(".song").html("hollyjollychristmas");
-		$(".artist").html("Michaelbuble");
-	};
-});
-
-$('.jcarousel-next').click(function() {
-    $('.jcarousel').jcarousel('scroll', '+=1');
-	
-		$(".audio-hollyjollychristmas")[0].pause();
-		$(".audio-alliwantforchristmas")[0].pause();
-		$(".audio-santababy")[0].pause();
-		$(".play i").removeClass("fa-pause").addClass("fa-play");
-		$(".play").removeClass("active")
-	
-				if ($('#jcarousel-item1').hasClass('active')) {
-		$('.alliwantforchirstmas, .santababy,erthakitt').removeClass('active');
-		$('.').addClass('active');
-		$(".song").html("alliwantforchirstmas");
-		$(".artist").html("mariahcary");
-	};
-	
-	if ($('#jcarousel-item2').hasClass('active')) {
-		$('.alliwantforchristmas, .santababy').removeClass('active');
-		$('.santababy').addClass('active');
-		$(".song").html("santababy");
-		$(".artist").html("erhtakitt");
-	};
-	
-	if ($('#jcarousel-item3').hasClass('active')) {
-		$('.alliwantforchristmas, .santababy').removeClass('active');
-		$('.hollyjollychristmas').addClass('active');
-		$(".song").html("hollyjollychristmas");
-		$(".artist").html("michelbuble");
-	};
-});
-
-$(".options a").click(function() {
-	$(this).toggleClass("active");
-});
-
-$(".favorite").click(function() {
-	if($(".options .favorite i").hasClass("fa-heart")) {
-		$(".options .favorite i").removeClass("fa-heart").addClass("fa-heart-o");
+$(function () {
+	var playerTrack = $("#player-track"),
+	  bgArtwork = $("#bg-artwork"),
+	  bgArtworkUrl,
+	  albumName = $("#album-name"),
+	  trackName = $("#track-name"),
+	  albumArt = $("#album-art"),
+	  sArea = $("#s-area"),
+	  seekBar = $("#seek-bar"),
+	  trackTime = $("#track-time"),
+	  insTime = $("#ins-time"),
+	  sHover = $("#s-hover"),
+	  playPauseButton = $("#play-pause-button"),
+	  i = playPauseButton.find("i"),
+	  tProgress = $("#current-time"),
+	  tTime = $("#track-length"),
+	  seekT,
+	  seekLoc,
+	  seekBarPos,
+	  cM,
+	  ctMinutes,
+	  ctSeconds,
+	  curMinutes,
+	  curSeconds,
+	  durMinutes,
+	  durSeconds,
+	  playProgress,
+	  bTime,
+	  nTime = 0,
+	  buffInterval = null,
+	  tFlag = false,
+	  albums = [
+		"All I Want For Christmas",
+		"Holly Jolly Christmas",
+		"Last Christmas",
+		"Santa Baby",
+		"Santa Tell Me"
+	  ],
+	  trackNames = [
+		"Mariah Carey- All I Want For Christmas",
+		"Michael Buble - Holly Jolly Christmas",
+		"Wham - Last Christmas",
+		"Eartha Kitt - Santa Baby",
+		"Ariana Grande - Santa Tell Me"
+	  ],
+	  albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
+	  trackUrl = [
+		"https://musicwetrust.blob.core.windows.net/audiocontainer/alliwantforchristmas.mp3",
+		"https://musicwetrust.blob.core.windows.net/audiocontainer/hollyjollychristmas.mp3",
+		"https://musicwetrust.blob.core.windows.net/audiocontainer/lastchristmas.mp3",
+		"https://musicwetrust.blob.core.windows.net/audiocontainer/santababy.mp3",
+		"https://musicwetrust.blob.core.windows.net/audiocontainer/Ariana-Grande---Santa-Tell-Me.mp3"
+	  ],
+	  playPreviousTrackButton = $("#play-previous"),
+	  playNextTrackButton = $("#play-next"),
+	  currIndex = -1;
+  
+	function playPause() {
+	  setTimeout(function () {
+		if (audio.paused) {
+		  playerTrack.addClass("active");
+		  albumArt.addClass("active");
+		  checkBuffering();
+		  i.attr("class", "fas fa-pause");
+		  audio.play();
+		} else {
+		  playerTrack.removeClass("active");
+		  albumArt.removeClass("active");
+		  clearInterval(buffInterval);
+		  albumArt.removeClass("buffering");
+		  i.attr("class", "fas fa-play");
+		  audio.pause();
+		}
+	  }, 300);
 	}
-	else {
-		$(".options .favorite i").removeClass("fa-heart-o").addClass("fa-heart");
+  
+	function showHover(event) {
+	  seekBarPos = sArea.offset();
+	  seekT = event.clientX - seekBarPos.left;
+	  seekLoc = audio.duration * (seekT / sArea.outerWidth());
+  
+	  sHover.width(seekT);
+  
+	  cM = seekLoc / 60;
+  
+	  ctMinutes = Math.floor(cM);
+	  ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
+  
+	  if (ctMinutes < 0 || ctSeconds < 0) return;
+  
+	  if (ctMinutes < 0 || ctSeconds < 0) return;
+  
+	  if (ctMinutes < 10) ctMinutes = "0" + ctMinutes;
+	  if (ctSeconds < 10) ctSeconds = "0" + ctSeconds;
+  
+	  if (isNaN(ctMinutes) || isNaN(ctSeconds)) insTime.text("--:--");
+	  else insTime.text(ctMinutes + ":" + ctSeconds);
+  
+	  insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
 	}
-});
-
-$(".play").click(function() {
-	$(".play").toggleClass("active");
-	if($(".play i").hasClass("fa-play")) {
-		$(".play i").removeClass("fa-play").addClass("fa-pause");
+  
+	function hideHover() {
+	  sHover.width(0);
+	  insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
 	}
-	else {
-		$(".play i").removeClass("fa-pause").addClass("fa-play");
+  
+	function playFromClickedPos() {
+	  audio.currentTime = seekLoc;
+	  seekBar.width(seekT);
+	  hideHover();
 	}
-	
-	if($(".play").hasClass("active") && $("#jcarousel-item3").hasClass("active")) {
-		var audio = $(".audio-hollyjollychristmas")[0];
-    audio.play();
-	} else {
-    var audio = $(".audio-hollyjollychristmas")[0];
-    audio.pause();
-    }
-	
-	if($(".play").hasClass("active") && $("#jcarousel-item2").hasClass("active")) {
-		var audio = $(".santababy")[0];
-    audio.play();
-	} else {
-    var audio = $(".santababy")[0];
-    audio.pause();
-    }
-	
-	if($(".play").hasClass("active") && $("#jcarousel-item1").hasClass("active")) {
-		var audio = $(".audio-alliwantforchristmas")[0];
-    audio.play();
-	} else {
-    var audio = $(".audio-alliwamtforchtistmas")[0];
-    audio.pause();
-    }
-});
-
-$(".volume").click(function(){
-		$(".volume").removeClass("active");
-		$(".volume-slider").animate({marginTop: '-150px'}, 500);
-});
-
-$(".volume-slider .close").click(function(){
-		$(".volume-slider").animate({marginTop: '0px'}, 500);
-})
-
-$(".side-menu-trigger").click(function(){
-    $(".side-menu").animate({marginLeft: '0px'});
-		$(".volume-slider").animate({marginTop: '0px'}, 500);
-}); 
-
-$(".side-menu li a, .side-menu .close").click(function(){
-    $(".side-menu").animate({marginLeft: '-310px'});
-}); 
-
-$('.volume-slider input[type="range"]').on('input', function () {
-            var percent = Math.ceil(((this.value - this.min) / (this.max - this.min)) * 100);
-            console.log(this.min);
-            $(this).css('background', '-webkit-linear-gradient(left, #e74c3c 0%, #e74c3c ' + percent + '%, #999 ' + percent + '%)');
-        });
-
-$(".volume-slider").slider({
-    min: 0,
-    max: 100,
-    value: 0,
-		range: "min",
-		animate: true,
-    slide: function(event, ui) {
-      setVolume((ui.value) / 100);
-    }
-});
-
-function setVolume(myVolume) {
-    var myMedia = document.getElementByClass('audio-avalanche');
-    myMedia.volume = myVolume;
-}
+  
+	function updateCurrTime() {
+	  nTime = new Date();
+	  nTime = nTime.getTime();
+  
+	  if (!tFlag) {
+		tFlag = true;
+		trackTime.addClass("active");
+	  }
+  
+	  curMinutes = Math.floor(audio.currentTime / 60);
+	  curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
+  
+	  durMinutes = Math.floor(audio.duration / 60);
+	  durSeconds = Math.floor(audio.duration - durMinutes * 60);
+  
+	  playProgress = (audio.currentTime / audio.duration) * 100;
+  
+	  if (curMinutes < 10) curMinutes = "0" + curMinutes;
+	  if (curSeconds < 10) curSeconds = "0" + curSeconds;
+  
+	  if (durMinutes < 10) durMinutes = "0" + durMinutes;
+	  if (durSeconds < 10) durSeconds = "0" + durSeconds;
+  
+	  if (isNaN(curMinutes) || isNaN(curSeconds)) tProgress.text("00:00");
+	  else tProgress.text(curMinutes + ":" + curSeconds);
+  
+	  if (isNaN(durMinutes) || isNaN(durSeconds)) tTime.text("00:00");
+	  else tTime.text(durMinutes + ":" + durSeconds);
+  
+	  if (
+		isNaN(curMinutes) ||
+		isNaN(curSeconds) ||
+		isNaN(durMinutes) ||
+		isNaN(durSeconds)
+	  )
+		trackTime.removeClass("active");
+	  else trackTime.addClass("active");
+  
+	  seekBar.width(playProgress + "%");
+  
+	  if (playProgress == 100) {
+		i.attr("class", "fa fa-play");
+		seekBar.width(0);
+		tProgress.text("00:00");
+		albumArt.removeClass("buffering").removeClass("active");
+		clearInterval(buffInterval);
+	  }
+	}
+  
+	function checkBuffering() {
+	  clearInterval(buffInterval);
+	  buffInterval = setInterval(function () {
+		if (nTime == 0 || bTime - nTime > 1000) albumArt.addClass("buffering");
+		else albumArt.removeClass("buffering");
+  
+		bTime = new Date();
+		bTime = bTime.getTime();
+	  }, 100);
+	}
+  
+	function selectTrack(flag) {
+	  if (flag == 0 || flag == 1) ++currIndex;
+	  else --currIndex;
+  
+	  if (currIndex > -1 && currIndex < albumArtworks.length) {
+		if (flag == 0) i.attr("class", "fa fa-play");
+		else {
+		  albumArt.removeClass("buffering");
+		  i.attr("class", "fa fa-pause");
+		}
+  
+		seekBar.width(0);
+		trackTime.removeClass("active");
+		tProgress.text("00:00");
+		tTime.text("00:00");
+  
+		currAlbum = albums[currIndex];
+		currTrackName = trackNames[currIndex];
+		currArtwork = albumArtworks[currIndex];
+  
+		audio.src = trackUrl[currIndex];
+  
+		nTime = 0;
+		bTime = new Date();
+		bTime = bTime.getTime();
+  
+		if (flag != 0) {
+		  audio.play();
+		  playerTrack.addClass("active");
+		  albumArt.addClass("active");
+  
+		  clearInterval(buffInterval);
+		  checkBuffering();
+		}
+  
+		albumName.text(currAlbum);
+		trackName.text(currTrackName);
+		albumArt.find("img.active").removeClass("active");
+		$("#" + currArtwork).addClass("active");
+  
+		bgArtworkUrl = $("#" + currArtwork).attr("src");
+  
+		bgArtwork.css({ "background-image": "url(" + bgArtworkUrl + ")" });
+	  } else {
+		if (flag == 0 || flag == 1) --currIndex;
+		else ++currIndex;
+	  }
+	}
+  
+	function initPlayer() {
+	  audio = new Audio();
+  
+	  selectTrack(0);
+  
+	  audio.loop = false;
+  
+	  playPauseButton.on("click", playPause);
+  
+	  sArea.mousemove(function (event) {
+		showHover(event);
+	  });
+  
+	  sArea.mouseout(hideHover);
+  
+	  sArea.on("click", playFromClickedPos);
+  
+	  $(audio).on("timeupdate", updateCurrTime);
+  
+	  playPreviousTrackButton.on("click", function () {
+		selectTrack(-1);
+	  });
+	  playNextTrackButton.on("click", function () {
+		selectTrack(1);
+	  });
+	}
+  
+	initPlayer();
+  });
+  
